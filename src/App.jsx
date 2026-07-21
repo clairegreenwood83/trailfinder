15ff6cc0336b5ad15f87783ce93be90f36a996dc
claire greenwood <claire.greenwood@arenaflowers.com>
2026-07-21 17:13:02 +0100

    Initial React and Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-5xl font-bold text-green-700">
+        TrailFinder 🌲
+      </h1>
+    </div>
+  )
+}
+
+export default App
